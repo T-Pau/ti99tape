f0f271e1f0bca5ac63a60349116437ec3f0fc6c9
Dieter Baron <dillo@nih.at>
2021-05-30 15:06:50 +0200

    Initial Commit

---
+++ ti99tape/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ti99tape
+//
+//  Created by Dieter Baron on 2021/05/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
